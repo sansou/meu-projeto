a63a52e7611ae45a3cc80a4d3d2682cc84817fe8
Thales Souza <santos.thales89@gmail.com>
2023-07-05 00:30:05 -0300

    commit inicial

--- src/App.js
+++ src/App.js
@@ -2,22 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = 'Thales'
+  const newName = name.toUpperCase()
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Alterando o JSX</h2>
+      <p>Olá, {newName}</p>
+      <p>Soma: {2 + 3}</p>
     </div>
   );
 }
