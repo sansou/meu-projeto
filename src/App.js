84397458cc6d64900b697c21d487b10cd0b31b02
Thales Souza <santos.thales89@gmail.com>
2023-07-05 00:59:36 -0300

    Trabalhando com props - #05

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
 import HelloWorld from './components/HelloWords'
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
 
 function App() {
-  const name = 'Thales'
-  const newName = name.toUpperCase()
+
 
   return (
     <div className="App">
-      <h2>Alterando o JSX</h2>
-      <p>Olá, {newName}</p>
-      <p>Soma: {2 + 3}</p>
-      <HelloWorld/>
+      <HelloWorld />
+      <SayMyName nome="Thales"></SayMyName>
+      <SayMyName nome="Vluzrmos"></SayMyName>
+      <Pessoa nome="Thales" idade="30" profissao="Programador" foto="http://via.placeholder.com/150"></Pessoa>
     </div>
   );
 }
---
+++ src/components/SayMyName.js
@@ -0,0 +1,11 @@
+function SayMyName(props) {
+
+  return(
+    <div>
+      <p>fala aí {props.nome}, suave?</p>
+    </div>
+  )
+  
+}
+
+export default SayMyName
